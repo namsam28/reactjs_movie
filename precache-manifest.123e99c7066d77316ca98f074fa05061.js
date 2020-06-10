a945ac67697145c3d6eb85a9d835a5e37144645b
namsam28 <namsam275@gmail.com>
2020-06-10 23:13:25 +0900

    Updates

---
+++ precache-manifest.123e99c7066d77316ca98f074fa05061.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d8bda4282f612873f5d6d6ba1ba30f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "640accc7c44ca19ad83d",
+    "url": "/static/css/main.1e061297.chunk.css"
+  },
+  {
+    "revision": "2dca63e70ba2a9074624",
+    "url": "/static/js/2.dc4e425c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.dc4e425c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "640accc7c44ca19ad83d",
+    "url": "/static/js/main.9026555a.chunk.js"
+  },
+  {
+    "revision": "b8e6a6a583046155bacb",
+    "url": "/static/js/runtime-main.c6288f8f.js"
+  }
+]);
